219bf2c6959a23f420f03d48bc162dfb65877cff
rohit-gupta-03 <rohitguptazack@gmail.com>
2025-08-27 10:41:49 +0530

    Intial Commit 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,10 +3,10 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 class TestModelTraining(unittest.TestCase):
-	def text_model_training(self):
-		model= joblib.load('model/iris_model.pkl')
-		self.assertIsInstance(model, RandomForestClassifier)
-		self.assertGreaterEqual(len(model.feature_importances_),4)
+	def test_model_training(self):
+   		 model = joblib.load('model/iris_model.pkl')
+   		 self.assertIsInstance(model, RandomForestClassifier)
+    		self.assertGreaterEqual(len(model.feature_importances_), 4)
 
 if __name__ == '__main__' :
 	unittest.main()
