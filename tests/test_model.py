29e71310220f64193028984e67be4cb8e7dff76c
rohit-gupta-03 <rohitguptazack@gmail.com>
2025-08-27 10:52:42 +0530

    Intial Commit 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,8 +4,8 @@ import joblib
 
 class TestModelTraining(unittest.TestCase):
 	def test_model_training(self):
-   		 model = joblib.load('model/iris_model.pkl')
-   		 self.assertIsInstance(model, RandomForestClassifier)
+		model = joblib.load('model/iris_model.pkl')
+		self.assertIsInstance(model, RandomForestClassifier)
     		self.assertGreaterEqual(len(model.feature_importances_), 4)
 
 if __name__ == '__main__' :
